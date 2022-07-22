251c612da047a2bd3435033781fb9d56d1b0916b
Choi ByungJae <wlvvwhs@naver.com>
2022-07-22 22:09:27 +0900

    0722 flex 관련 transform

---
+++ src/main/java/javascript/mission3/Javascript5Application.java
@@ -0,0 +1,13 @@
+package javascript.mission3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javascript5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javascript5Application.class, args);
+    }
+
+}
---
+++ src/test/java/javascript/mission3/Javascript5ApplicationTests.java
@@ -0,0 +1,13 @@
+package javascript.mission3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javascript5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
